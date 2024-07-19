578c15560340fb87b105262f49a5e4249fbe86de
Lesha <bobkov0987654321@gmail.com>
2024-07-19 07:44:13 +0300

    easy debug

--- main.gd
+++ main.gd
@@ -2,12 +2,8 @@ extends Node2D
 
 @onready var player_scene = load("res://player.tscn")
 const PORT = 8088
-const GODOT_PATH = "4.3.exe"
-const CLIENT_COUNT = 2
 func _ready() -> void:
-	if OS.has_feature("editor"):
-		debug_clients()
-	var peer = WebSocketMultiplayerPeer.new()
+	var peer = ENetMultiplayerPeer.new()
 	if DisplayServer.get_name() == "headless":
 		print("server started")
 		peer.create_server(PORT)
@@ -15,15 +11,10 @@ func _ready() -> void:
 		multiplayer.peer_connected.connect(add_player)
 	else:
 		print("client started")
-		peer.create_client("ws://127.0.0.1:"+str(PORT))
+		peer.create_client("127.0.0.1", PORT)
 		multiplayer.multiplayer_peer = peer
 
 func add_player(id: int) -> void:
 	var player = player_scene.instantiate()
 	player.name = str(id)
 	add_child(player)
-
-func debug_clients():
-	OS.execute("CMD.exe", ["/C", GODOT_PATH+' --export-debug "Windows Desktop" ./client.exe'])
-	for i in range(CLIENT_COUNT):
-		OS.create_process("client.exe", [], true)
